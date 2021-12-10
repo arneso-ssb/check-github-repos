9c643badd47226f96d64b8eaa9fc34b56b74d19b
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2021-12-10 16:28:06 +0100

    Add logging
    - Store repo most comitter name and email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import shutil
 import stat
@@ -11,6 +12,17 @@ from git import Repo
 from github import Github
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("jupyter-output-check.log", mode="w"),
+        logging.StreamHandler(),
+    ],
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
 @dataclass
 class RepoStatistics:
     repo_name: str
@@ -18,6 +30,8 @@ class RepoStatistics:
     dirty_branches: int = 0
     dirty_files: int = 0
     error_files: int = 0
+    repo_contact_name: str = ""
+    repo_contact_email: str = ""
 
 
 def credentials_url(url, token):
@@ -33,7 +47,7 @@ def check_file(file, repo_stat):
     try:
         subprocess.run(argv, check=True)
     except subprocess.CalledProcessError:
-        print(f"  ERROR from nbstripout command on {file}")
+        logging.warning(f"ERROR from nbstripout command on {file}")
         repo_stat.error_files += 1
     return repo_stat
 
@@ -50,61 +64,70 @@ def delete_dir(path):
 
 
 def check_branch(git_repo, path, branch, repo_stat):
-    print(f"Checking branch: {branch}")
     git_repo.git.switch(branch)
-
     files_to_check = path.rglob("*.ipynb")
-    # print("  Files to check for output:")
+    logging.debug("  Files to check for output:")
+
     for file in files_to_check:
-        # print(f"    {file}")
+        logging.debug(f"    {file}")
         repo_stat = check_file(file, repo_stat)
 
     changed_files = [item.a_path for item in git_repo.index.diff(None)]
     if changed_files:
-        print("  Files that contains output:")
+        logging.warning(
+            f"  Checking branch: {branch}, DIRTY. Repo: {repo_stat.repo_name}. Files that contains output:"
+        )
         for file in changed_files:
-            print(f"    {file}")
+            logging.warning(f"      {file}")
         repo_stat.state = "DIRTY"
         repo_stat.dirty_branches += 1
         repo_stat.dirty_files += len(changed_files)
+    else:
+        logging.info(f"  Checking branch: {branch}, CLEAN")
 
     # Cleanup
     git_repo.git.reset("--hard")
 
-    print(f"  Branch {branch} is DIRTY") if changed_files else print(
-        f"  Branch {branch} is CLEAN"
-    )
     return repo_stat
 
 
 def check_repo(repo, token):
     clone_url = credentials_url(repo.clone_url, token)
-    print("--------------------------------------------")
-    print(f"Checking repo: {repo.full_name}")
-    # print(f"Checking repo: {repo.full_name}, {repo.name}, {repo.clone_url}")
+    logging.info("--------------------------------------------")
+    logging.info(f"Checking repo: {repo.full_name}")
+
     # Set clone path to ../tmp-repos/{repo.name}
     path = Path().resolve().parent / "tmp-repos" / repo.name
     delete_dir(path)
     git_repo = Repo.clone_from(clone_url, path)
 
+    # Strip "origin/"-part of branch names
     branches = [b.name[7:] for b in git_repo.remote().refs]
     if "HEAD" in branches:
         branches.remove("HEAD")
-    print(f"Branches: {branches}")
+    logging.info(f"  Branches: {branches}")
 
     repo_stat = RepoStatistics(repo.full_name)
     for branch in branches:
         repo_stat = check_branch(git_repo, path, branch, repo_stat)
 
-    print(f"Repo {repo.full_name} is {repo_stat.state}")
-    print(repo_stat)
+    if repo_stat.state == "CLEAN":
+        logging.info(f"Repo {repo.full_name} is CLEAN")
+    else:
+        logging.warning(f"Repo {repo.full_name} is {repo_stat.state}")
+        contributors = repo.get_contributors()
+        if contributors:
+            repo_stat.repo_contact_name = contributors[0].name
+            repo_stat.repo_contact_email = contributors[0].email
 
-    # delete_dir(path)
+    logging.info(repo_stat)
+
+    delete_dir(path)
     return repo_stat
 
 
 def main(token):
-    print("Scanning organization for repos containing Jupyter Notebooks...")
+    logging.info("Scanning organization for repos containing Jupyter Notebooks...")
     g = Github(token)
 
     # jupyter_repos = [
@@ -114,21 +137,29 @@ def main(token):
     jupyter_repos = []
     for repo in g.get_organization("statisticsnorway").get_repos():
         if "Jupyter Notebook" in repo.get_languages():
-            print(f"{repo.full_name} contains notebooks")
+            logging.info(f"{repo.full_name} contains notebooks")
             jupyter_repos.append(repo)
-    print(f"There are {len(jupyter_repos)} repos with Jupyter Notebooks")
+    logging.info(f"There are {len(jupyter_repos)} repos with Jupyter Notebooks")
 
     repo_stats = []
     for repo in jupyter_repos:
         repo_stats.append(check_repo(repo, token))
 
-    print("------------------------------------")
-    print("Statistics:")
-    print(f"Number of Jupyter repos : {len(repo_stats)}")
-    print(f"Number of dirty repos   : {sum(rs.state == 'DIRTY' for rs in repo_stats)}")
-    print(f"Number of dirty branches: {sum(rs.dirty_branches for rs in repo_stats)}")
-    print(f"Number of dirty files   : {sum(rs.dirty_files for rs in repo_stats)}")
-    print(f"Number of error files   : {sum(rs.error_files for rs in repo_stats)}")
+    logging.info("------------------------------------")
+    logging.info("Statistics:")
+    logging.info(f"Number of Jupyter repos : {len(repo_stats)}")
+    logging.info(
+        f"Number of dirty repos   : {sum(rs.state == 'DIRTY' for rs in repo_stats)}"
+    )
+    logging.info(
+        f"Number of dirty branches: {sum(rs.dirty_branches for rs in repo_stats)}"
+    )
+    logging.info(
+        f"Number of dirty files   : {sum(rs.dirty_files for rs in repo_stats)}"
+    )
+    logging.info(
+        f"Number of error files   : {sum(rs.error_files for rs in repo_stats)}"
+    )
 
 
 if __name__ == "__main__":
